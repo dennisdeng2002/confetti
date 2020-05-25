9cb43e5e986a77303035c5601ba58bff0bb63602
Dennis Deng <dennisdeng2002@yahoo.com>
2020-05-24 21:25:35 -0400

    Remove OnAttachStateChangeListener in ConfettiManager, update Gradle plugin version to 3.6.3

--- confetti/src/main/java/com/github/jinatonic/confetti/ConfettiManager.java
+++ confetti/src/main/java/com/github/jinatonic/confetti/ConfettiManager.java
@@ -88,18 +88,6 @@ public class ConfettiManager {
         this.parentView = parentView;
         this.confettiView = confettiView;
         this.confettiView.bind(confetti);
-
-        this.confettiView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
-            @Override
-            public void onViewAttachedToWindow(View v) {
-            }
-
-            @Override
-            public void onViewDetachedFromWindow(View v) {
-                terminate();
-            }
-        });
-
         // Set the defaults
         this.ttl = -1;
         this.bound = new Rect(0, 0, parentView.getWidth(), parentView.getHeight());
